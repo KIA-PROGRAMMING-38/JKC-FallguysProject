99de8dfccedb0a84b420847a6624695c75467a00
주진수 <120005202+weweweme@users.noreply.github.com>
2023-06-16 02:15:40 +0900

    [Chore] 폴더 정리 및, READ.ME 파일 수정

--- JKC_Fallguys/Assets/1_Scripts/Scene/02_MatchingStandby/MatchingStandbySceneInitializer.cs
+++ JKC_Fallguys/Assets/1_Scripts/Scene/02_MatchingStandby/MatchingStandbySceneInitializer.cs
@@ -1,7 +1,7 @@
 using LiteralRepository;
 using UnityEngine;
 
-public class MatchingStandbySceneInitializer : SceneInitialize
+public class MatchingStandbySceneInitializer : SceneInitializer
 {
     [Header("Object")]
     private LineEffectPooler _lineEffectPooler;
