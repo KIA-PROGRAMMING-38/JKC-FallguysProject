8e13d74c471a5b7dbe8e1e60864c074079413a3f
Huiji_MacMini <gmlwl0408@gmail.com>
2023-06-16 00:06:53 +0900

    [Feat] 1,2,3등 위치에 폴가이 프리팹 생성

--- JKC_Fallguys/Assets/Huiji/Scripts/RoundResultScene/ResultRoundSetupManager.cs
+++ JKC_Fallguys/Assets/Huiji/Scripts/RoundResultScene/ResultRoundSetupManager.cs
@@ -8,23 +8,26 @@ public class ResultRoundSetupManager : MonoBehaviour
     // 프리팹 불러오기
     private GameObject[] _fallGuy = new GameObject[3];
     
-    // 큐에 골에 들어간 플레이어 인원담기
+    // 생성될 위치랑 애니메이터 배열로 선언하기
+    private Vector3[] _fallGuyPositions = new[]
+    {
+        new Vector3(-3.17f, 2.25f, 0f),
+        new Vector3(3.96f, 0.25f, 0f),
+        new Vector3(11.185f, -2.04f, 0f)
+    };
+    
     private void Awake()
     {
         for (int fallGuyIndex = 0; fallGuyIndex < _fallGuy.Length; ++fallGuyIndex)
         {
             _fallGuy[fallGuyIndex] = DataManager.GetGameObjectData(
-                DataManager.SetDataPath(PathLiteral.Prefabs, PathLiteral.Scene, "05_RoundResult", "RoundResultFallGuy")); 
+                DataManager.SetDataPath(PathLiteral.Prefabs, PathLiteral.Scene, "05_RoundResult", "RoundResultFallGuy"));
+
+            Instantiate(_fallGuy[fallGuyIndex], _fallGuyPositions[fallGuyIndex],
+                _fallGuy[fallGuyIndex].transform.rotation);
         }
     }
 
-    
-    private void Start()
-    {
-        
-    }
-    
-    // 생성될 위치랑 애니메이터 배열로 선언하기
     // 반복문을 돌면서 프리팹 생성하기
     // 생성할때 위치랑 애니메이터 할당하기
 }
