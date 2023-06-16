c281a3b65bdbde1875c254100ad7efb06db23258
Huiji_MacMini <gmlwl0408@gmail.com>
2023-06-16 21:46:20 +0900

    [Feat] Round Result Scene -> GameLoading Scene

---
+++ JKC_Fallguys/Assets/Huiji/Scripts/RoundResultScene/SceneChanger.cs
@@ -0,0 +1,20 @@
+using System;
+using Cysharp.Threading.Tasks;
+using LiteralRepository;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private void Start()
+    {
+        LoadGameLoadingScene();
+    }
+
+    private async UniTaskVoid LoadGameLoadingScene()
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(10));
+
+        SceneManager.LoadScene(PathLiteral.GameLoading);
+    } 
+}
