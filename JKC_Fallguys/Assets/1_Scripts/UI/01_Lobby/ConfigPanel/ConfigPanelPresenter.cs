55b2103ff15b4a01828a313d27112db102f56acd
Huiji_MacMini <gmlwl0408@gmail.com>
2023-06-19 16:19:12 +0900

    [Style] Config Panel MVRP 스크립트 추가

---
+++ JKC_Fallguys/Assets/1_Scripts/UI/01_Lobby/ConfigPanel/ConfigPanelPresenter.cs
@@ -0,0 +1,22 @@
+public class ConfigPanelPresenter : Presenter
+{
+    public override void OnInitialize(View view)
+    {
+        
+    }
+
+    public override void OnRelease()
+    {
+        
+    }
+
+    protected override void OnOccuredUserEvent()
+    {
+        
+    }
+
+    protected override void OnUpdatedModel()
+    {
+        
+    }
+}
