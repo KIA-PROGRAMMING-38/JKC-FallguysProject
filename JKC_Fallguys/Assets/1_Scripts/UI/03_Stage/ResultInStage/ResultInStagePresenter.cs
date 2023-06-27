4a473e29565709684aa09c0c2ddf50c9ea6e6a48
Huiji_MacMini <gmlwl0408@gmail.com>
2023-06-28 02:46:12 +0900

    [Feat] ResultInStage Panel 완성

--- JKC_Fallguys/Assets/1_Scripts/UI/03_Stage/ResultInStage/ResultInStagePresenter.cs
+++ JKC_Fallguys/Assets/1_Scripts/UI/03_Stage/ResultInStage/ResultInStagePresenter.cs
@@ -1,8 +1,10 @@
+using UniRx;
 using UnityEngine;
 
 public class ResultInStagePresenter : Presenter
 {
     private ResultInStageView _resultInStageView;
+    private CompositeDisposable _compositeDisposable = new CompositeDisposable();
     public override void OnInitialize(View view)
     {
         _resultInStageView = view as ResultInStageView;
@@ -15,13 +17,39 @@ public class ResultInStagePresenter : Presenter
         
     }
 
+    private Vector2 _center = new Vector2(0.5f, 0.5f);
     protected override void OnUpdatedModel()
     {
-        
+        StageDataManager.Instance.CurrentState
+            // .Skip(1) // 초기 Default로 설정된것은 Skip.
+            .Subscribe(currentState =>
+            {
+                switch (currentState)
+                {
+                    case StageDataManager.PlayerState.Success:
+                        _resultInStageView.ResultText.text = "성공!";
+                        break;
+                    case StageDataManager.PlayerState.Defeat:
+                        _resultInStageView.ResultText.text = "실패!";
+                        break;
+                    case StageDataManager.PlayerState.End:
+                        _resultInStageView.ResultText.text = "종료!";
+                        break;
+                    default:
+                        Debug.Log("PlayerState를 설정해야 합니다.");
+                        break;
+                }
+
+                _resultInStageView.ResultPanel.MoveUI(_center, _resultInStageView.CanvasRect, 0.5f);
+                
+            })
+            .AddTo(_compositeDisposable);
     }
+
     
     public override void OnRelease()
     {
-        
+        _resultInStageView = default;
+        _compositeDisposable.Dispose();
     }
 }
--- JKC_Fallguys/Assets/1_Scripts/UI/03_Stage/ResultInStage/ResultInStageView.cs
+++ JKC_Fallguys/Assets/1_Scripts/UI/03_Stage/ResultInStage/ResultInStageView.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 
 public class ResultInStageView : View
 {
+    public RectTransform CanvasRect { get; private set; }
+    public RectTransform ResultPanel { get; private set; }
     public Text ResultText { get; private set; }
+    
     private void Awake()
     {
-        ResultText = transform.Find("Background").Find("ResultText").GetComponent<Text>();
+        CanvasRect = GetComponent<RectTransform>();
+        Debug.Assert(CanvasRect != null);
+        ResultPanel = transform.Find("ResultPanel").GetComponent<RectTransform>();
+        Debug.Assert(ResultPanel != null);
+        ResultText = transform.Find("ResultPanel").Find("ResultText").GetComponent<Text>();
         Debug.Assert(ResultText != null);
     }
 }
