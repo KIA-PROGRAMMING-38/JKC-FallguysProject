e9a585e9348a86316a73ecba483b8a21189c65ac
주진수 <120005202+weweweme@users.noreply.github.com>
2023-06-27 21:15:07 +0900

    [Feat] HoopLegend 맵의 Hoop를 통과할 시, 파티클 이미지 추가

--- JKC_Fallguys/Assets/1_Scripts/Stage/HoopLegend/CommonHoop.cs
+++ JKC_Fallguys/Assets/1_Scripts/Stage/HoopLegend/CommonHoop.cs
@@ -1,10 +1,13 @@
-using System.Collections;
+using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class CommonHoop : MonoBehaviour
 {
     private HoopController _hoopController;
-    
+    private ParticleSystem _passEffect;
+    private ParticleSystem _passEffectReverse;
+
     private GoalCheck _goalCheck;
     private Vector3 _originPosition;
     private bool _isMoving;
@@ -18,6 +21,12 @@ public class CommonHoop : MonoBehaviour
     {
         _goalCheck = transform.Find("GoalCheck").GetComponent<GoalCheck>();
         Debug.Assert(_goalCheck != null);
+        _passEffect = transform.Find("PassEffect").GetComponent<ParticleSystem>();
+        _passEffectReverse = transform.Find("PassEffectReverse").GetComponent<ParticleSystem>();
+        _passEffect.Stop();
+        _passEffectReverse.Stop();
+        Debug.Assert(_passEffect != null);
+        Debug.Assert(_passEffectReverse != null);
         
         _originPosition = transform.position;
         _currentHeight = _originPosition.y;
@@ -39,10 +48,22 @@ public class CommonHoop : MonoBehaviour
         if (!_playerEntered && !_isMoving)
         {
             _playerEntered = true;
-            StartCoroutine(PropellerAnimation());
+            ParticlePlay().Forget();
+            PropellerAnimation().Forget();
             
             _hoopController.PlayerPassesHoop(1);
         }
+    }
+
+    private async UniTaskVoid ParticlePlay()
+    {
+        _passEffect.Play();
+        _passEffectReverse.Play();
+
+        await UniTask.Delay(TimeSpan.FromSeconds(2f));
+        
+        _passEffect.Stop();
+        _passEffectReverse.Stop();
     }
 
     private void HandlePlayerExit()
@@ -50,7 +71,7 @@ public class CommonHoop : MonoBehaviour
         _playerEntered = false;
     }
 
-    private IEnumerator PropellerAnimation()
+    private async UniTaskVoid PropellerAnimation()
     {
         _isMoving = true;
         float targetHeight = _originPosition.y + _heightOffset;
@@ -61,10 +82,11 @@ public class CommonHoop : MonoBehaviour
             elapsedTime += Time.deltaTime * moveSpeedMultiplier;
             _currentHeight = Mathf.Lerp(_originPosition.y, targetHeight, elapsedTime / _duration);
             transform.position = new Vector3(_originPosition.x, _currentHeight, _originPosition.z);
-            yield return null;
+
+            await UniTask.Yield();
         }
 
-        yield return new WaitForSeconds(_duration);
+        await UniTask.Delay(TimeSpan.FromSeconds(_duration));
 
         elapsedTime = 0f;
         while (_currentHeight > _originPosition.y)
@@ -72,16 +94,13 @@ public class CommonHoop : MonoBehaviour
             elapsedTime += Time.deltaTime * moveSpeedMultiplier;
             _currentHeight = Mathf.Lerp(targetHeight, _originPosition.y, elapsedTime / _duration);
             transform.position = new Vector3(_originPosition.x, _currentHeight, _originPosition.z);
-            yield return null;
+            
+            await UniTask.Yield();
         }
 
         _isMoving = false;
     }
 
-    private void PlayerHoopCount()
-    {
-    }
-    
     private void OnDestroy()
     {
         _goalCheck.OnPlayerEnter -= HandlePlayerEnter;
