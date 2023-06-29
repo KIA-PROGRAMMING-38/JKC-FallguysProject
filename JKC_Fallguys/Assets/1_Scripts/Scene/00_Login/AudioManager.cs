51298fefe7b61e23979496f0ab24377a134db08a
Huiji_MacMini <gmlwl0408@gmail.com>
2023-06-30 01:19:31 +0900

    [Feat] AudioSoucres 두개로 나눠서 관리

--- JKC_Fallguys/Assets/1_Scripts/Scene/00_Login/AudioManager.cs
+++ JKC_Fallguys/Assets/1_Scripts/Scene/00_Login/AudioManager.cs
@@ -3,17 +3,37 @@ using UnityEngine;
 
 public class AudioManager : SingletonMonoBehaviour<AudioManager>
 {
+    // 외부에서 사용할 AudioSource
     public AudioSource MusicAudioSource { get; private set; }
+    
+    // Intro와 LoopMusic을 담당할 Audio Sources
+    private AudioSource[] _audioSources = new AudioSource[2];
+    
     private AudioClip _loginSound;
 
     protected override void Awake()
     {
         base.Awake();
-        
-        gameObject.AddComponent<AudioSource>();
-        MusicAudioSource = GetComponent<AudioSource>();
 
-        MusicAudioSource.clip = AudioRegistry.LobbyMusic;
+        for (int index = 0; index < _audioSources.Length; ++index)
+        {
+            _audioSources[index] = gameObject.AddComponent<AudioSource>();
+            
+            // 클립 두개 저장해야함.
+            //ScheduleAudioClip(_audioSources[index], );
+        }
+        
+        // MusicAudioSource에 Play할 AudioSource 할당해야 함.
+    }
+    
+    // 오디오 클립을 예약하여 재생하는 메서드
+    void ScheduleAudioClip(AudioSource audioSource ,AudioClip clip, double startTime, bool loop)
+    {
+        audioSource.clip = clip;
+        audioSource.loop = loop; // 루프 여부 설정
+
+        // 오디오 클립을 예약하여 재생
+        audioSource.PlayScheduled(startTime);
     }
 
     public void DestorySelf()
--- JKC_Fallguys/Assets/1_Scripts/Util/ResourceRegistry/AudioRegistry.cs
+++ JKC_Fallguys/Assets/1_Scripts/Util/ResourceRegistry/AudioRegistry.cs
@@ -13,11 +13,9 @@ namespace ResourceRegistry
         private static void SetAudioClips()
         {
             LoginSFX = DataManager.GetAudioClip(PathLiteral.Sounds, PathLiteral.Music, PathLiteral.LoginSound);
-            LobbyMusic = DataManager.GetAudioClip(PathLiteral.Sounds, PathLiteral.Music, PathLiteral.Lobby,
-                "Mus_MainMenu_Loop");
         }
         
         public static AudioClip LoginSFX { get; private set; }
-        public static AudioClip LobbyMusic { get; private set; }
+        public static AudioClip[] LobbyMusic { get; private set; }
     }
 }
