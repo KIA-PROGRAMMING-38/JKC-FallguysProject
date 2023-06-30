1718b820a9149101e670feafeb4c20f57ac30b74
Huiji_MacMini <gmlwl0408@gmail.com>
2023-06-30 21:29:26 +0900

    [Feat] AudioScheduler 추상 클래스 구현

---
+++ JKC_Fallguys/Assets/1_Scripts/System/AudioScheduler.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public abstract class AudioScheduler : MonoBehaviour
+{
+    protected virtual void Start()
+    {
+        SetMusicScheduleTime();
+        
+        PlayAudio();
+    }
+    
+    /// <summary>
+    /// 음악이 시작되는 시간을 설정합니다. 
+    /// </summary>
+    public abstract void SetMusicScheduleTime();
+
+    /// <summary>
+    /// 스케줄을 짠 오디오 소스를 플레이 합니다. ScheduleAudioSource 메소드를 사용해서 스케줄을 짜주세요.
+    /// </summary>
+    public abstract void PlayAudio();
+    
+    protected void ScheduleAudioSource(AudioSource audioSource, double startTime, bool loop)
+    {
+        audioSource.loop = loop;
+        
+        audioSource.PlayScheduled(startTime);
+    }
+}
