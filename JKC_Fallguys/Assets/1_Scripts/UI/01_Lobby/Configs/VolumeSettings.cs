9646f52c16113ff162ba7835a2f3bed5b59ad2cb
yoreleihee <120005259+yoreleihee@users.noreply.github.com>
2023-07-06 02:04:04 +0900

    [Feat] VolumeValue Text 구현

--- JKC_Fallguys/Assets/1_Scripts/UI/01_Lobby/Configs/VolumeSettings.cs
+++ JKC_Fallguys/Assets/1_Scripts/UI/01_Lobby/Configs/VolumeSettings.cs
@@ -18,21 +18,44 @@ public class VolumeSettings : MonoBehaviour
         _audioMixer = Resources.Load<AudioMixer>( DataManager.SetDataPath( PathLiteral.Sounds, PathLiteral.AudioMixer ) );
         _audioMixerParameterName = transform.parent.name;
     }
-
+    
     private void Start()
     {
         _volumeSlider.onValueChanged.AddListener(SetVolume);
-        _volumeSlider.value = PlayerPrefs.GetFloat(_audioMixerParameterName, 1f);
+        
+        LoadConfigsValue();
+        
+        // Login Scene에서 작성해야 함.
         _audioMixer.SetFloat(_audioMixerParameterName, Mathf.Log10(_volumeSlider.value) * 20);
+    }
+
+    private void LoadConfigsValue()
+    {
+        _volumeSlider.value = PlayerPrefs.GetFloat(_audioMixerParameterName, 1f);
+        _volumeValue.text = PlayerPrefs.GetString(_audioMixerParameterName + "_Text", "100");
     }
 
     private void SetVolume(float volume)
     {
         _audioMixer.SetFloat( _audioMixerParameterName, Mathf.Log10( volume ) * 20 );
+        
+        DisplayVolumeValueText(volume);
+    }
+
+    private void DisplayVolumeValueText(float volume)
+    {
+        int displayValue = Mathf.RoundToInt(volume * 100);
+        _volumeValue.text = displayValue.ToString();
     }
 
     private void OnDisable()
     {
+        SaveConfigsValue();
+    }
+
+    private void SaveConfigsValue()
+    {
         PlayerPrefs.SetFloat(_audioMixerParameterName, _volumeSlider.value);
+        PlayerPrefs.SetString(_audioMixerParameterName + "_Text", _volumeValue.text);
     }
 }
