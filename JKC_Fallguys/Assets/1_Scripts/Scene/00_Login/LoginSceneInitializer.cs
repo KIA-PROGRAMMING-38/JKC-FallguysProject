abddee004e8100395d6f3fc92556faa6801d180c
HuiJi <gmlwl0408@gmail.com>
2023-07-06 21:38:09 +0900

    [Bug] 게임을 실행할때 AudioMixer의 값이 Slider의 값으로 설정되지 않음

--- JKC_Fallguys/Assets/1_Scripts/Scene/00_Login/LoginSceneInitializer.cs
+++ JKC_Fallguys/Assets/1_Scripts/Scene/00_Login/LoginSceneInitializer.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using LiteralRepository;
+using ResourceRegistry;
 
 public class LoginSceneInitializer : SceneInitializer
 {
     protected override void Awake()
     {
         base.Awake();
-        
+
+        float volume = PlayerPrefs.GetFloat("MusicVolume");
+        AudioRegistry.GameAudioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+
         Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
     }
 
