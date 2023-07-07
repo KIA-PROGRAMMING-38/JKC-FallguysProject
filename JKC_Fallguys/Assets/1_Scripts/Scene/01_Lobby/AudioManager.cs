6618667c74c3b584997cb3465588879a31554ebc
HuiJi <gmlwl0408@gmail.com>
2023-07-07 17:56:32 +0900

    [Refac] AudioManager가 AudioSource를 플레이 하게 변경

--- JKC_Fallguys/Assets/1_Scripts/Scene/01_Lobby/AudioManager.cs
+++ JKC_Fallguys/Assets/1_Scripts/Scene/01_Lobby/AudioManager.cs
@@ -6,24 +6,38 @@ using UnityEngine.Audio;
 public class AudioManager : SingletonMonoBehaviour<AudioManager>
 {
     // Intro와 LoopMusic을 담당할 Audio Sources
-    private AudioSource[] _audioSources = new AudioSource[2];
+    private AudioSource[] _musicAudioSources = new AudioSource[2];
+    private AudioSource _musicIntro => _musicAudioSources[0];
+    private AudioSource _musicLoop => _musicAudioSources[1];
     
     // 외부에서 사용할 AudioSource
-    public AudioSource[] MusicAudioSource => _audioSources;
+    public AudioSource[] MusicAudioSource => _musicAudioSources;
 
 
     protected override void Awake()
     {
         base.Awake();
 
-        for (int index = 0; index < _audioSources.Length; ++index)
+        for (int index = 0; index < _musicAudioSources.Length; ++index)
         {
-            _audioSources[index] = gameObject.AddComponent<AudioSource>();
-            _audioSources[index].volume = 0.3f;
+            _musicAudioSources[index] = gameObject.AddComponent<AudioSource>();
+            _musicAudioSources[index].volume = 0.3f;
 
-            _audioSources[index].outputAudioMixerGroup = AudioRegistry.GameAudioMixer.FindMatchingGroups( "Music" )[0];
+            _musicAudioSources[index].outputAudioMixerGroup = AudioRegistry.GameAudioMixer.FindMatchingGroups( "Music" )[0];
         }
 
+        _musicIntro.loop = false;
+        _musicLoop.loop = true;
+
         gameObject.AddComponent<AudioReverbZone>().reverbPreset = AudioReverbPreset.Room;
+    }
+
+    private void Start()
+    {
+        _musicIntro.clip = AudioRegistry.LobbyMusic[0];
+        _musicLoop.clip = AudioRegistry.LobbyMusic[1];
+
+        _musicIntro.Play();
+        _musicLoop.PlayDelayed(AudioRegistry.LobbyMusic[0].length);
     }
 }
