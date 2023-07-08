9e7d95660a19a6588c27935b095ca7c120ff359f
yoreleihee <120005259+yoreleihee@users.noreply.github.com>
2023-07-08 21:19:33 +0900

    [Feat] AudioManager Init

--- JKC_Fallguys/Assets/1_Scripts/Scene/01_Lobby/AudioManager.cs
+++ JKC_Fallguys/Assets/1_Scripts/Scene/01_Lobby/AudioManager.cs
@@ -1,43 +1,39 @@
+using System;
 using LiteralRepository;
 using ResourceRegistry;
 using UnityEngine;
 using UnityEngine.Audio;
 
+public enum SoundType
+{
+    MusicIntro,
+    MusicLoop,
+    SFX
+}
+
 public class AudioManager : SingletonMonoBehaviour<AudioManager>
 {
-    // Intro와 LoopMusic을 담당할 Audio Sources
-    private AudioSource[] _musicAudioSources = new AudioSource[2];
-    private AudioSource _musicIntro => _musicAudioSources[0];
-    private AudioSource _musicLoop => _musicAudioSources[1];
-    
-    // 외부에서 사용할 AudioSource
-    public AudioSource[] MusicAudioSource => _musicAudioSources;
-
+    private AudioSource[] _audioSources;
 
     protected override void Awake()
     {
         base.Awake();
-
-        for (int index = 0; index < _musicAudioSources.Length; ++index)
-        {
-            _musicAudioSources[index] = gameObject.AddComponent<AudioSource>();
-            _musicAudioSources[index].volume = 0.3f;
-
-            _musicAudioSources[index].outputAudioMixerGroup = AudioRegistry.GameAudioMixer.FindMatchingGroups( "Music" )[0];
-        }
-
-        _musicIntro.loop = false;
-        _musicLoop.loop = true;
-
-        gameObject.AddComponent<AudioReverbZone>().reverbPreset = AudioReverbPreset.Room;
+        
+        Init();
     }
 
-    private void Start()
+    private void Init()
     {
-        _musicIntro.clip = AudioRegistry.LobbyMusic[0];
-        _musicLoop.clip = AudioRegistry.LobbyMusic[1];
+        string[] soundTypeNames = Enum.GetNames(typeof(SoundType));
+        _audioSources = new AudioSource[soundTypeNames.Length];
+        for (int i = 0; i < soundTypeNames.Length; ++i)
+        {
+            GameObject go = new GameObject(soundTypeNames[i]);
+            go.transform.parent = transform;
+            _audioSources[i] = go.AddComponent<AudioSource>();
+        }
 
-        _musicIntro.Play();
-        _musicLoop.PlayDelayed(AudioRegistry.LobbyMusic[0].length);
+        AudioSource musicLoop = _audioSources[(int)SoundType.MusicLoop];
+        musicLoop.loop = true;
     }
 }
